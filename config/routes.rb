8f133b9109498c26f808adf873c27f93b41c9e79
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-24 23:12:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'products/add', to: 'products#add'
   root to: "products#index"
+  post 'products/add', to: 'products#add'
+
 end
